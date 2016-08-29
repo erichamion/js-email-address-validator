fb3165efef21c9de0d746a42098ab7e23fb23cfb
erichamion <erichamion@gmail.com>
2016-08-28 21:04:33 -0500

    Added more tests

--- tests/validate-email.tests.js
+++ tests/validate-email.tests.js
@@ -89,7 +89,7 @@ QUnit.test('validateEmailAddressFormat_TooLong_Fail', function (assert) {
     var result = validateEmailAddressFormat(addr);
 
     // Assert
-    QUnit.assert.notOk(result, 'Invalid long address should fail');
+    assert.notOk(result, 'Invalid long address should fail');
 });
 
 QUnit.test('validateEmailAddressFormat_LocalTooLong_Fail', function (assert) {
@@ -102,5 +102,129 @@ QUnit.test('validateEmailAddressFormat_LocalTooLong_Fail', function (assert) {
     var result = validateEmailAddressFormat(addr);
 
     // Assert
-    QUnit.assert.notOk(result, 'Address with over 64 characters in local part should fail');
+    assert.notOk(result, 'Address with over 64 characters in local part should fail');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalPartHasValidSymbols_Pass', function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    var addr = "!#$%&'*+-/=?^_`{|}~@example.com";
+
+    // Act
+    var result = validateEmailAddressFormat(addr);
+
+    // Assert
+    assert.ok(result, 'Address with valid symbols in the local part should pass');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalPartHasDots_Pass', function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    var addr = "abc.def.ghi.j.k@example.com";
+
+    // Act
+    var result = validateEmailAddressFormat(addr);
+
+    // Assert
+    assert.ok(result, 'Address with nonconsecutive . characters in the local part should pass');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalPartHasConsecutiveDots_Fail', function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    var addr = "abc..def@example.com";
+
+    // Act
+    var result = validateEmailAddressFormat(addr);
+
+    // Assert
+    assert.notOk(result, 'Address with consecutive . characters in the local part should fail');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalPartStartsWithDot_Fail', function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    var addr = ".abc@example.com";
+
+    // Act
+    var result = validateEmailAddressFormat(addr);
+
+    // Assert
+    assert.notOk(result, 'Address with local part starting with . character should fail');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalPartEndsWithDot_Fail', function (assert) {
+    assert.expect(1);
+
+    // Arrange
+    var addr = "abc.@example.com";
+
+    // Act
+    var result = validateEmailAddressFormat(addr);
+
+    // Assert
+    assert.notOk(result, 'Address with local part ending with . character should fail');
+});
+
+QUnit.test('validateEmailAddressFormat_LocalHasInvalidSymbol_Fail', function (assert) {
+
+    // Arrange
+    var addresses = [
+        'abc@def@example.com',
+        '@abc@example.com',
+        'abc@@example.com',
+        'ab"c"d@example.com',
+        'abc[@example.com',
+        'abc]@example.com',
+        'abc[]def@example.com',
+        'abc[def]ghi@example.com',
+        'abc def@example.com',
+        'abc\ndef@example.com'
+        ];
+    var results = [];
+
+    // Act
+    addresses.forEach(function(addr) {
+        results.push({address:addr, result:validateEmailAddressFormat(addr)});
+    }); 
+
+    // Assert
+    assert.expect(addresses.length);
+    results.forEach(function(result) {
+        assert.notOk(result.result, '"' + result.address + '" has invalid characters in the local part and should fail');
+    })
+});
+
+QUnit.test('validateEmailAddressFormat_LocalhasQuotedSymbols_Pass', function (assert) {
+
+    // Arrange
+    var addresses = [
+        '"abc"@example.com',
+        '"abc@def"@example.com',
+        '"@abc".def@example.com',
+        'xyz."abc@"@example.com',
+        '"abc["@example.com',
+        'xyz."abc]".def@example.com',
+        '"abc[]def"@example.com',
+        '"abc[def]ghi"@example.com',
+        '"abc def"@example.com',
+        '"abc\ndef"@example.com',
+        '"abc....def"@example.com',
+        ];
+    var results = [];
+
+    // Act
+    addresses.forEach(function(addr) {
+        results.push({address:addr, result:validateEmailAddressFormat(addr)});
+    }); 
+
+    // Assert
+    assert.expect(addresses.length);
+    results.forEach(function(result) {
+        assert.ok(result.result, '"' + result.address + '" has a quoted section in the local part and should pass');
+    })
 });
