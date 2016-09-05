7e4f408d0ada50c6601583bbd4ec1e8b7213560f
erichamion <erichamion@gmail.com>
2016-09-04 21:58:27 -0500

    Added code comments

--- scripts/email-validator.js
+++ scripts/email-validator.js
@@ -17,6 +17,7 @@ function EmailValidator(options) {
     }
 }
 
+
 EmailValidator.prototype = {
 
     // Helper functions
@@ -49,13 +50,26 @@ EmailValidator.prototype = {
     
 
     // Constant or constant-like values
+    
+    // RFC 5322 3.2.3: atext = ALPHA / DIGIT / "!" / "#" / "$" / "%" / "&" / "'" / "*" / "+" / "-" / "/" / "=" / "?" / "^" / "_" / "`" / "{" / "|" / "}" / "~"
+    // Printable low-ASCII characters with a number of specific special characters omitted
     _atext: String.raw`[\`\-a-zA-Z0-9!#$%&'*+/=?^_{|}~]`,
-    _wsp: '( |\t)'
+    
+    // RFC 5322 pulls the WSP definition from RFC 5234.
+    // RFC 5234 Appendix B.1: WSP = SP / HTAB
+    // Space or (horizontal) tab
+    _wsp: '( |\t)',
 };
 
 
 function defineFws(allowObsolete) {
+    // RFC 5322 3.2.2: FWS = ([*WSP CRLF] 1*WSP) / obs-FWS
+    // Ignoring the obsolete syntax, this is zero or one newline embedded anywhere in a sequence
+    // of one or more whitespace characters, as long as the newline is not final character.
     var strictFws = '((' + this._wsp + '*' + String.raw`\n)?` + this._wsp + '+)';
+    // RFC 5322 4.2: obs-FWS = 1*WSP *(CRLF 1*WSP)
+    // This adds the possibility of multiple newlines, as long as each one is followed by at least
+    // one whitespace character, and as long as the sequence does not start with a newline.
     var obsFws = '(' + this._wsp + String.raw`+(\n` + this._wsp + '+)*)';
     return allowObsolete ?
             this._makeAlternatives(strictFws, obsFws) :
